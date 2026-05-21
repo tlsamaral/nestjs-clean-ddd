1b81d978d0769e1a29c2438a0f339143d93e1e9d
Talles <worktalles@hotmail.com>
2026-05-21 10:12:30 -0300

    chore(main): update bootstrap configuration

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,16 +1,16 @@
-import { ConfigService } from "@nestjs/config";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import type { Env } from "./env/env";
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import type { Env } from './env/env'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		// logger: false,
-	});
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  })
 
-	const configService = app.get<ConfigService<Env, true>>(ConfigService);
-	const port = configService.get("PORT", { infer: true });
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', { infer: true })
 
-	await app.listen(port);
+  await app.listen(port)
 }
-bootstrap();
+bootstrap()
