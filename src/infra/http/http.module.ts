d74e13a34e8551846ebbead73533abae200b4534
Talles <worktalles@hotmail.com>
2026-05-21 10:12:30 -0300

    refactor(http): update http module providers and controllers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { CryptographyModule } from '../cryptography/cryptography.module'
+import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
-  controllers: [AuthenticateController],
-  providers: [PrismaService],
+  imports: [DatabaseModule, CryptographyModule],
+  controllers: [
+    AuthenticateController,
+    CreateAccountController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [
+    AuthenticateStudentUseCase,
+    RegisterStudentUseCase,
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+  ],
 })
 export class HttpModule {}
